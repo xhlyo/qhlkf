85bda64bebd50dc3be265fb196aa5893500d9137
xhlyo <xhlkfyo@sohu.com>
2021-03-01 17:27:31 +0800

    2021.3.1- 17:27

---
+++ vue2_college/src/views/layout/components/sendItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="send-container">发表动态的地方</div>  
+</template>
+
+<script>
+export default {
+  name: 'send',
+  components: {}, 
+  props: {}, 
+  data () {
+    return {
+        
+    }
+  }, 
+  computed: {}, 
+  watch: {}, 
+  created () {}, 
+  mounted () {}, 
+  methods: {}, 
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
